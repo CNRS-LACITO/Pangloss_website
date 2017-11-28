3fa0e9e73412531cf79c1e5fa7eb62d60f5ea7fe
alexis-michaud <michaud.cnrs@gmail.com>
2017-11-28 12:48:49 +0100

    remplacement page LANGUE unique 'Maleng' (groupe) par 5 pages, 1 par langue, x 2 (en + fr)
    Pour mise en ligne :-)

---
+++ languages/Malieng_en.php
@@ -0,0 +1,136 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Pangloss Collection - Malieng (M&atilde; Liềng) corpus</title>
+</head>
+<body>
+<div>&nbsp;</div>
+<div class="page-header">
+<h1>Malieng (M&atilde; Liềng) corpus</h1>
+</div>
+<div class="text-left">
+<p class="intertitre"> <a href="#Vietnamese"><span
+ style="font-style: italic;">Giới
+thiệu bằng tiếng Việt</span></a></p>
+<p>&nbsp;</p>
+See pp. 15-16 of:
+Ferlus, Michel. 1996. <a
+ href="http://sealang.net/sala/archives/pdf8/ferlus1996langues.pdf"
+ target="_blank">Langues et peuples viet-muong</a>.
+<span style="font-style: italic;">Mon-Khmer
+Studies</span> 26. 7&ndash;28. <br>
+<p class="intertitre"><strong></strong></p>
+<p class="intertitre"><strong>The
+archived documents: vocabulary</strong></p>
+<p>The recordings made
+available here were conducted by Michel Ferlus, in collaboration with
+Pr. Trần Tr&iacute; D&otilde;i (Vietnam National University).
+They&nbsp;contain lexical data. As Ferlus's field notes have not
+been
+verified item by item to finalize the word list, it was decided to put
+the data online without a transcription, rather than run the risk of
+publishing data that contain mistaken
+transcriptions. Michel Ferlus rated the technical quality of the
+Aheu recordings as "very poor"; this is
+due to the use of low-grade tapes (counterfeit tapes sold on the local
+market).</p>
+<br>
+<p style="font-weight: bold;">References
+and links to
+related resources:&nbsp;</p>
+Ferlus, Michel. 1996. <a
+ href="http://sealang.net/sala/archives/pdf8/ferlus1996langues.pdf"
+ target="_blank">Langues et peuples viet-muong</a>.
+<span style="font-style: italic;">Mon-Khmer
+Studies</span> 26, pp.
+7&ndash;28.<br>
+<span style="font-style: italic;"></span>
+<p>The
+EFEO-CNRS-SOAS
+Word List for Linguistic Fieldwork in
+Southeast Asia, used to elicit the vocabulary, is available <a
+ href="https://halshs.archives-ouvertes.fr/halshs-01068533/"
+ target="_blank">here</a>.</p>
+<p></p>
+<p class="intertitre"><span style="font-style: italic;">Classification
+of the
+Vietic languages (a.k.a. Viet-Muong, Việt-Chứt)
+after Ferlus 1996, with links to the corresponding pages in the
+Pangloss Collection:</span>&nbsp;</p>
+<table style="text-align: left;" ;="" border="1" cellpadding="2"
+ cellspacing="2" width="100%">
+  <tbody>
+    <tr>
+      <td align="undefined" valign="undefined">1. Maleng
+(this page)</td>
+      <td align="undefined" valign="undefined">2. Arem</td>
+      <td align="undefined" valign="undefined">3. Chứt</td>
+      <td align="undefined" valign="undefined">4. Aheu</td>
+      <td align="undefined" valign="undefined">5. Hung</td>
+      <td align="undefined" valign="undefined">6. Thổ</td>
+      <td align="undefined" valign="undefined">7. Mường</td>
+      <td align="undefined" valign="undefined">8. Vietnamese</td>
+    </tr>
+  </tbody>
+</table>
+<p class="intertitre"></p>
+<p></p>
+<p style="font-style: italic;">These
+materials were digitized and edited as part of a project <span
+ style="font-style: italic;">(Sept. 2014-Feb. 2016) </span>funded
+by the <a href="http://www.bibliothequescientifiquenumerique.fr/"
+ target="_blank">Digital Scientific Library</a><span
+ style="font-style: italic;">, a programme of the
+French Ministry for Higher
+Education and Research.&nbsp;</span></p>
+<a href="http://www.bibliothequescientifiquenumerique.fr/"
+ target="_blank"> <img src="../../images/logos/bsn.png"
+ alt="Digital Scientific Library" border="0" height="75"></a>
+<p>Last updated 11/2017.</p>
+<p><a href="../../membres/michaud.htm">Contact: Alexis
+Michaud</a></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<h1 align="center"><a name="Vietnamese"></a>Ngữ
+liệu tiếng Maleng (Maleng, M&atilde;liềng v&agrave; Kha Phong)</h1>
+<strong>C&aacute;c
+t&agrave;i liệu được lưu trữ: ngữ li&ecirc;̣u v&ecirc;̀ từ
+vựng</strong>
+<p>Phần ghi
+&acirc;m ở đ&acirc;y bao gồm c&aacute;c dữ liệu về từ
+vựng. T&agrave;i liệu tiếng Rục được sưu tầm trong sự hợp
+t&aacute;c
+v&oacute;i GS. TS. Trần Tr&iacute; D&otilde;i (VNU).</p>
+<br>
+<p style="font-weight: bold;">T&agrave;i
+liệu tham khảo:&nbsp;</p>
+Bảng từ
+EFEO-CNRS-SOAS d&ugrave;ng trong c&ocirc;ng t&aacute;c điền
+d&atilde; ng&ocirc;n ngữ học ở Đ&ocirc;ng Nam &Aacute;
+(tải bảng từ <a
+ href="https://halshs.archives-ouvertes.fr/halshs-01068533/"
+ target="_blank">ở đ&acirc;y</a>).
+<br>
+<p> <span style="font-style: italic;">C&aacute;c
+tư liệu về tiếng Chứt đ&atilde; được số h&oacute;a
+v&agrave; bi&ecirc;n tập lại trong khu&ocirc;n khổ của một
+đề t&agrave;i k&eacute;o d&agrave;i từ
+th&aacute;ng 9/2014 đến th&aacute;ng 2/2016, với ng&acirc;n
+s&aacute;ch từ chương tr&igrave;nh Thư
+viện Khoa học Kỹ thuật
+số <a href="http://www.bibliothequescientifiquenumerique.fr/"
+ target="_blank">(Digital Scientific Library)</a>,
+một chương
+tr&igrave;nh của Bộ Gi&aacute;o
+dục đại học v&agrave;
+Nghi&ecirc;n cứu Ph&aacute;p.&nbsp;</span></p>
+<a href="http://www.bibliothequescientifiquenumerique.fr/"
+ target="_blank"> <img src="../../images/logos/bsn.png"
+ alt="Digital Scientific Library" border="0" height="75"></a>
+<p style="font-style: italic;"><span style="font-style: italic;"></span></p>
+</div>
+</body>
+</html>
--- languages/Pakatan.php
+++ languages/Pakatan.php
@@ -1,55 +1,36 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Collection Pangloss - corpus pakatan</title>
+</head>
+<body>
 <div>&nbsp;</div>
-
-<html><head><meta charset="utf-8"/><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>
-           <div class="page-header"><h1>Corpus
-maleng (maleng bro, pakatan, malang, m&atilde;liềng, kha phong)</h1></div>
+<div class="page-header">
+<h1>Corpus&nbsp;pakatan</h1>
+</div>
 <div class="text-left">
-            <p class="intertitre">&nbsp;</p>
-            <h4 align="left">Ressources
-:</h4>
-            <p class="intertitre"> <img
- src="../../images/images_pangloss/haut_parleur_s.png" alt="a" height="20" width="20"><a
- href="../corpus/list_rsc_en.php?lg=Maleng%20Br%C3%B4&amp;aff=Maleng%20Br%C3%B4"
- target="_blank"> Acc&eacute;der au vocabulaire Maleng
-Br&ocirc;&nbsp;(8 documents)</a></p>
-            <p class="intertitre"> <img
- src="../../images/images_pangloss/haut_parleur_s.png" alt="a" height="20" width="20"><a
- href="../corpus/list_rsc_en.php?lg=Pakatan&amp;aff=Pakatan" target="_blank">
-Acc&eacute;der au vocabulaire Pakatan&nbsp;(4 documents)</a></p>
-            <p class="intertitre"> <img
- src="../../images/images_pangloss/haut_parleur_s.png" alt="a" height="20" width="20"><a
- href="../corpus/list_rsc_en.php?lg=Malang&amp;aff=Malang" target="_blank">
-Acc&eacute;der au vocabulaire Malang&nbsp;(4 documents)</a></p>
-            <p class="intertitre"> <img
- src="../../images/images_pangloss/haut_parleur_s.png" alt="a" height="20" width="20"><a
- href="../corpus/list_rsc_en.php?lg=Malieng&amp;aff=Malieng" target="_blank">
-Acc&eacute;der au vocabulaire Malieng (M&atilde;liềng)&nbsp;(3
-documents)</a></p>
-            <p class="intertitre"> <img
- src="../../images/images_pangloss/haut_parleur_s.png" alt="a" height="20" width="20"><a
- href="../corpus/list_rsc_en.php?lg=Kha%20Phong&amp;aff=Kha%20Phong"
- target="_blank"> Acc&eacute;der au vocabulaire Kha Phong&nbsp;(4
-documents)</a></p>
-            <p class="intertitre" align="left"><a
- href="../corpus/list_rsc.php?lg=M%C6%B0%E1%BB%9Dng&amp;aff=m%C6%B0%E1%BB%9Dng"
- target="_blank"></a> </p>
-            <p class="intertitre" align="left">Au sein du groupe
+<p class="intertitre">&nbsp;</p>
+Au sein du groupe
 vietique, le
-sous-groupe maleng comporte trois zones dialectales</p>
-            <p class="intertitre" align="left">1) Maleng, (Kha)
+sous-groupe maleng comporte trois zones dialectales
+<p class="intertitre" align="left">1)
+Maleng, (Kha)
 Pakatan, Malang,
 Har&egrave;me (Rivi&egrave;re 1902).&nbsp;<span
  style="font-style: italic;">Autres noms:&nbsp;</span>Kha
 Muong Ben et Kha Bo (Fraisse 1950).</p>
-            <p class="intertitre" align="left">2) M&atilde;liềng. <span
- style="font-style: italic;">Autre nom:</span>
+<p class="intertitre" align="left">2)
+M&atilde;liềng. <span style="font-style: italic;">Autre nom:</span>
 Pa Leng (Đặng Nghi&ecirc;m Vạn et al. 1986).</p>
-            <p class="intertitre" align="left">3) Kha Phong, Maleng
+<p class="intertitre" align="left">3)
+Kha Phong, Maleng
 Kari, Maleng
 Br&ocirc;. <span style="font-style: italic;">Autre nom: </span>Kha
 Nam Om (Fraisse 1949)</p>
-            <p class="intertitre" align="left">"L'ethnonyme <span
- style="font-style: italic;">Maleng</span>
+<p class="intertitre" align="left">"L'ethnonyme
+<span style="font-style: italic;">Maleng</span>
 [<span style="font-weight: bold;">măləɛ̀ŋ&sup2;</span>]
 signifie "&Ecirc;tre humain" et sert d'autonyme &agrave; la
 quasi totalit&eacute; des communaut&eacute;s de ce sous-groupe.
@@ -74,7 +55,6 @@ particuli&egrave;rement dans le Nord, a une connotation
 extr&ecirc;mement p&eacute;jorative &agrave; un point tel
 qu'il est aujourd'hui proscrit de l'usage. L'autonyme <span
  style="font-style: italic;">Malang</span>
-
 n'est attest&eacute; que dans un seul village; peut-&ecirc;tre
 s'est-il substitu&eacute; &agrave; <span style="font-style: italic;">Maleng</span>
 pour la m&ecirc;me raison que <span style="font-style: italic;">Pakatan</span>.
@@ -96,8 +76,8 @@ Bo</span>
 dispers&eacute;s entre la Nam Theun et le M&eacute;kong ne sont
 pas d'anciens <span style="font-style: italic;">Maleng</span>
 qui se seraient laocis&eacute;s.&nbsp;</p>
-            <p class="intertitre" align="left">Les <span
- style="font-style: italic;">M&atilde;liềng</span>
+<p class="intertitre" align="left">Les
+<span style="font-style: italic;">M&atilde;liềng</span>
 (forme vietnamis&eacute;e de la prononciation de <span
  style="font-style: italic;">Maleng</span>)
 constituent une avanc&eacute;e du sous-groupe en territoire
@@ -109,8 +89,9 @@ repr&eacute;sent&eacute;s et qu'ils devaient former le fonds de
 la population dans cette r&eacute;gion. Leur langue est fortement
 marqu&eacute;e par un dialecte vietnamien &agrave; quatre tons
 qui n'est plus parl&eacute; dans leur voisinage.</p>
-            <p class="intertitre" align="left">Les <span
- style="font-style: italic;">Kha Phong</span>
+<p class="intertitre" align="left">Les
+<span style="font-style: italic;">Kha
+Phong</span>
 (ancien exonyme devenu autonyme) ne subsistent plus que par deux ou
 trois villages dans la haute r&eacute;gion du
 c&ocirc;t&eacute; lao de la fronti&egrave;re et par un
@@ -131,120 +112,106 @@ du Vietnam se disent aussi <span style="font-style: italic;">Maleng
 kari</span>
 [<span style="font-weight: bold;">măləɛŋ&sup2;
 kăriː&sup1;</span>]
-dans lequel <span style="font-style: italic;">kari</span> peut
+dans lequel <span style="font-style: italic;">kari</span>
+peut
 &ecirc;tre rapproch&eacute; de <span style="font-style: italic;">Chali</span>
-et de <span style="font-style: italic;">Tri</span>, noms donn&eacute;s
+et de <span style="font-style: italic;">Tri</span>,
+noms donn&eacute;s
 par ailleurs &agrave; certains groupes <span
  style="font-style: italic;">S&ocirc;-Bru</span>.&nbsp;</p>
-            <p class="intertitre" align="left">Dans l'ensemble, ces
+<p class="intertitre" align="left">Dans
+l'ensemble, ces
 populations pratiquent une agriculture de montagne. Les <span
- style="font-style: italic;">M&atilde;liềng</span> du Vietnam sont
-estim&eacute;s &agrave; quelque 200 locuteurs. Nous ne connaissons pas
-les chiffres concernant le&nbsp;Laos mais on peut penser que le total
+ style="font-style: italic;">M&atilde;liềng</span>
+du Vietnam sont
+estim&eacute;s &agrave; quelque 200 locuteurs. Nous ne
+connaissons pas
+les chiffres concernant le&nbsp;Laos mais on peut penser que le
+total
 des locuteurs de tout le sous-groupe <span style="font-style: italic;">Maleng</span>
 ne doit gu&egrave;re d&eacute;passer le millier."&nbsp;</p>
-            <p style="text-align: right;" class="intertitre">(Pages
+<p style="text-align: right;" class="intertitre">(Pages
 12-13 de: Ferlus, Michel.
 1996. <a
  href="http://sealang.net/sala/archives/pdf8/ferlus1996langues.pdf"
- target="_blank">Langues et peuples viet-muong</a>. <span
- style="font-style: italic;">Mon-Khmer
+ target="_blank">Langues et peuples viet-muong</a>.
+<span style="font-style: italic;">Mon-Khmer
 Studies</span> 26. 7&ndash;28.)</p>
-            <p class="intertitre">&nbsp;</p>
-            <p class="intertitre"><span style="font-style: italic;">Classification
+<p class="intertitre">&nbsp;</p>
+<p class="intertitre"><span style="font-style: italic;">Classification
 des
 langues viet-muong (vietiques) d'apr&egrave;s Ferlus 1996, et lien
 vers les pages correspondantes dans la Collection Pangloss :</span>&nbsp;</p>
-            <table style="text-align: left;" ;="" border="1"
- cellpadding="2" cellspacing="2" width="100%">
-              <tbody>
-                <tr>
-                  <td align="undefined" valign="undefined">1. Maleng
+<table style="text-align: left;" ;="" border="1" cellpadding="2"
+ cellspacing="2" width="100%">
+  <tbody>
+    <tr>
+      <td align="undefined" valign="undefined">1. Maleng
 (cette page)</td>
-                  <td align="undefined" valign="undefined">2. <a
- href="Arem.htm" target="_blank">Arem</a></td>
-                  <td align="undefined" valign="undefined">3. <a
- href="Chut.htm" target="_blank">Chứt</a></td>
-                  <td align="undefined" valign="undefined">4. <a
- href="Aheu.htm" target="_blank">Aheu</a></td>
-                  <td align="undefined" valign="undefined">5. <a
- href="Hung.htm" target="_blank">Hung</a></td>
-                  <td align="undefined" valign="undefined">6. <a
- href="Tho.htm" target="_blank">Thổ</a></td>
-                  <td align="undefined" valign="undefined">7. <a
- href="Muong.htm" target="_blank">Mường</a></td>
-                  <td align="undefined" valign="undefined">8. <a
- href="Vietnamese.htm" target="_blank">Vietnamien</a></td>
-                </tr>
-              </tbody>
-            </table>
-            <p>&nbsp;</p>
-            <p style="font-style: italic;">Ces
+      <td align="undefined" valign="undefined">2. Arem</td>
+      <td align="undefined" valign="undefined">3. Chứt</td>
+      <td align="undefined" valign="undefined">4. Aheu</td>
+      <td align="undefined" valign="undefined">5. Hung</td>
+      <td align="undefined" valign="undefined">6. Thổ</td>
+      <td align="undefined" valign="undefined">7. Mường</td>
+      <td align="undefined" valign="undefined">8. Vietnamien</td>
+    </tr>
+  </tbody>
+</table>
+<p>&nbsp;</p>
+<p style="font-style: italic;">Ces
 donn&eacute;es&nbsp;ont
 &eacute;t&eacute;
 num&eacute;ris&eacute;es et
-catalogu&eacute;es <span
- style="font-style: italic;">par l'<a
+catalogu&eacute;es <span style="font-style: italic;">par l'<a
  href="http://www.mica.edu.vn/" target="_blank">Institut
 de recherche international MICA</a> (</span><i>HUST
 &ndash; CNRS/UMI-2954 &ndash; Grenoble INP)</i><span
  style="font-style: italic;"> en partenariat avec le <a
  href="http://lacito.vjf.cnrs.fr/" target="_blank">LACITO</a></span>
-dans le cadre du <a
- href="https://lacito.hypotheses.org/251" target="_blank">projet
-            </a><span
- style="font-style: italic;"><a
+dans le cadre du <a href="https://lacito.hypotheses.org/251"
+ target="_blank">projet
+</a><span style="font-style: italic;"><a
  href="https://lacito.hypotheses.org/251" target="_blank">DO-RE-MI-FA</a>
 (Sept. 2014-Feb. 2016) </span>financ&eacute;
-par la&nbsp;<a
- href="http://www.bibliothequescientifiquenumerique.fr/"
+par la&nbsp;<a href="http://www.bibliothequescientifiquenumerique.fr/"
  target="_blank">Biblioth&egrave;que Scientifique
-Num&eacute;rique</a><span
- style="font-style: italic;">
+Num&eacute;rique</a><span style="font-style: italic;">
 (BSN), un programme du Minist&egrave;re de l'Enseignement
-Sup&eacute;rieur et de la Recherche (MESR, France)</span><i>.</i><i></i><span
- style="font-style: italic;"></span><span
- style="font-style: italic;"></span><span
+Sup&eacute;rieur et de la Recherche (MESR, France)</span><i>.</i><span
+ style="font-style: italic;"></span><span style="font-style: italic;"></span><span
  style="font-style: italic;"></span></p>
-            <a
- href="http://www.bibliothequescientifiquenumerique.fr/"
+<a href="http://www.bibliothequescientifiquenumerique.fr/"
  target="_blank"> <br>
-            </a>
-            <table
+</a>
+<table
  style="width: 800px; height: 90px; text-align: left; margin-left: auto; margin-right: auto;"
  border="0" cellpadding="2" cellspacing="2">
-              <tbody>
-                <tr>
-                  <td
- style="text-align: center; height: 35%; width: 35%;"><a
+  <tbody>
+    <tr>
+      <td style="text-align: center; height: 35%; width: 35%;"><a
  href="http://www.bibliothequescientifiquenumerique.fr/"><span
  style="text-decoration: underline;"><img
- style="border: 0px solid ; width: 241px; height: 64px;"
- alt="BSN"
+ style="border: 0px solid ; width: 241px; height: 64px;" alt="BSN"
  src="http://bbf.enssib.fr/consulter/bbf-2013-1-p61.jpg"></span></a></td>
-                  <td
- style="text-align: center;"><a
+      <td style="text-align: center;"><a
  href="http://www.mica.edu.vn/index.php"><img
- style="border: 0px solid ; width: 80px; height: 74px;"
- alt="MICA"
+ style="border: 0px solid ; width: 80px; height: 74px;" alt="MICA"
  src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Logo-mica-transp.gif"></a></td>
-                  <td
- style="text-align: center;">
-                  <p><a
+      <td style="text-align: center;">
+      <p><a
  href="http://www.mica.edu.vn/research-actions/au-co-project/"><img
- style="border: 0px solid ; width: 109px; height: 50px;"
- alt="AuCo"
+ style="border: 0px solid ; width: 109px; height: 50px;" alt="AuCo"
  src="https://upload.wikimedia.org/wikipedia/commons/f/ff/Logo_of_the_AuCo_collection%2C_a_project_of_the_MICA_International_Research_Institute_in_Hanoi%2C_Vietnam.jpg"></a></p>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <p style="font-style: italic;"><span
- style="font-style: italic;"></span></p>
-            <p>&nbsp;</p>
-            <p>Mise &agrave;
-jour : 2015.</p>
-            <p><a href="../../membres/michaud.htm">Contact: Alexis
+      </td>
+    </tr>
+  </tbody>
+</table>
+<p style="font-style: italic;"><span style="font-style: italic;"></span></p>
+<p>&nbsp;</p>
+<p>Mise &agrave;
+jour :&nbsp;11/2017.</p>
+<p><a href="../../membres/michaud.htm">Contact: Alexis
 Michaud</a></p>
 </div>
 </body>
