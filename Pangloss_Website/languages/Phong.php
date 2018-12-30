2129af03abdbfc78033fb43eb435427e94d08c5e
alexis-michaud <michaud.cnrs@gmail.com>
2018-12-30 12:52:27 +0100

    création pages pour langues LYHA, PHONG, TOUM, TAYPOONG
    et suppression du groupe "Hung".
    En vue résolution "Issue" #75

---
+++ Pangloss_Website/languages/Phong.php
@@ -0,0 +1,133 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head> <meta charset="utf-8"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><div>&nbsp;</div>
+
+
+           <div class="page-header"><h1>Corpus
+hung (dialectes: toum,&nbsp;ly hà, tày poọng, phong)</h1></div>
+           <div class="text-left"> <p class="intertitre">&nbsp;</p>
+            <h4 style="font-style: italic;" align="left">Ce corpus a été ouvert au public en 2019, sous licence Creative Commons.</h4>
+Au sein du groupe
+vietique, le
+sous-groupe hung comporte deux zones dialectales: pong (poọng) / phong
+/ tay pong d'une part ; toum, đan lai, ly hà (liha) de l'autre.
+            <p style="text-align: justify;" class="intertitre">"Des
+parlers de ce
+sous-groupe ont
+été connus dès le début du
+[XXe] siècle grâce aux collectes
+inédites du Père Th. Guignard (1907) sur le <span style="font-style: italic;">Hung</span>
+et le <span style="font-style: italic;">Không
+Khêng</span>. Ces deux
+termes sont aujourd'hui sortis de l'usage et c'est la raison pour
+laquelle nous avons retenu l'un des deux pour désigner le
+sous-groupe. Les deux vocabulaires, bien notés
+grâce aux qualités de lexicographe du
+missionnaire, couvrent en fait deux variantes d'une même
+langue dont les locuteurs se reconnaissent aujourd'hui sous l'autonyme
+de <span style="font-style: italic;">Pong</span>,
+en vietnamien Poọng [<span style="font-weight: bold;">pɔːŋ⁴</span>]
+et en lao <span style="font-style: italic;">Phong</span>.
+Le vocabulaire <span style="font-style: italic;">K'katiam-Pong-Houk</span>
+de Paul Macey (1907), hélas moins bien noté,
+représente également la même langue.
+Dans cette dernière expression <span style="font-style: italic;">K'katiam</span>
+doit pouvoir s'analyser en <span style="font-style: italic;">Kha Cham</span>,
+c'est-à-dire "les Kha de Muong Cham", tandis que dans <span style="font-style: italic;">Pong-Houk</span>
+seul le premier terme est identifiable. Le vocabulaire <span style="font-style: italic;">Toum Phong</span>
+utilisé par A.G. Haudricourt (1966) est, malgré
+son nom, également du pong. Les parlers des <span style="font-style: italic;">Toum</span>
+[<span style="font-weight: bold;">tuːm¹</span>],
+des <span style="font-style: italic;">Đan
+Lai</span> et des <span style="font-style: italic;">Ly Hà</span>
+n'ont été
+révélés que dans les années
+soixante (Mạc Đường 1964). Ils se différencient nettement du
+pong par plusieurs traits phonétiques et une influence
+récente du vietnamien. Les <span style="font-style: italic;">Pong</span>
+sont représentés des deux
+côtés de la frontière, dans le district
+de Tương Dương (Nghệ An, Vietnam), et dans la région de
+Muong Cham (Bolikhamxay, Laos). Les <span style="font-style: italic;">Toum</span>
+ne sont représentés que du
+côté lao où ils vivent
+dispersés au milieu des populations Tay dans le massif qui
+sépare les deux pays, tandis que les&nbsp;<span style="font-style: italic;">Đan Lai</span>
+et les <span style="font-style: italic;">Ly
+Hà</span>, de langue
+identique, sont surtout représentés dans le
+district de Con Cuông. Le Père Guignard rapporte
+qu'au début du siècle les <span style="font-style: italic;">Hung</span>
+étaient très nombreux dans la région
+et que le Chao Muong de Khamkeut était un des leurs.</p>
+            <p style="text-align: justify;" class="intertitre">Dans
+l'ensemble, ces
+populations
+pratiquent le même type d'agriculture que leurs voisins Lao
+ou Tay. Les <span style="font-style: italic;">Poọng</span>
+de Tương Dương ont été
+évalués à un peu plus de 700
+personnes. On ne dispose malheureusement pas de chiffres pour le Laos
+mais on peut penser que l'ensemble des locuteurs que nous avons
+dénommés <span style="font-style: italic;">Hung</span>
+doit pouvoir s'évaluer entre 2.000 et 3.000
+personnes."&nbsp;</p>
+            <p style="text-align: right;" class="intertitre">(Pages
+15-16 de: Ferlus, Michel.
+1996. <a href="http://sealang.net/sala/archives/pdf8/ferlus1996langues.pdf" target="_blank">Langues et peuples viet-muong</a>. <span style="font-style: italic;">Mon-Khmer
+Studies</span> 26. 7–28.)</p>
+            <p class="intertitre">&nbsp;</p>
+            <p class="intertitre"><span style="font-style: italic;">Classification
+des
+langues viet-muong (vietiques) d'après Ferlus 1996, et lien
+vers les pages correspondantes dans la Collection Pangloss :</span>&nbsp;</p>
+            <table style="text-align: left;" ;="" border="1" cellpadding="2" cellspacing="2" width="80%">
+              <tbody>
+                <tr>
+                  <td align="undefined" valign="undefined">1. <a href="Maleng.htm" target="_blank">Maleng</a></td>
+                  <td align="undefined" valign="undefined">2. <a href="Arem.htm" target="_blank">Arem</a></td>
+                  <td align="undefined" valign="undefined">3. <a href="Chut.htm" target="_blank">Chứt</a></td>
+                  <td align="undefined" valign="undefined">4. <a href="Aheu.htm" target="_blank">Aheu</a></td>
+                  <td align="undefined" valign="undefined">5. Hung
+(cette page)</td>
+                  <td align="undefined" valign="undefined">6. <a href="Tho.htm" target="_blank">Thổ</a></td>
+                  <td align="undefined" valign="undefined">7. <a href="Muong.htm" target="_blank">Mường</a></td>
+                  <td align="undefined" valign="undefined">8. <a href="Vietnamese.htm" target="_blank">Vietnamien</a></td>
+                </tr>
+              </tbody>
+            </table>
+           <p>&nbsp;</p>
+            <p style="font-style: italic;">Ces
+données&nbsp;ont
+été
+numérisées et
+cataloguées <span style="font-style: italic;">par l'<a href="http://www.mica.edu.vn/" target="_blank">Institut
+de recherche international MICA</a> (</span><i>HUST
+– CNRS/UMI-2954 – Grenoble INP)</i><span style="font-style: italic;"> en partenariat avec le <a href="http://lacito.vjf.cnrs.fr/" target="_blank">LACITO</a></span>
+dans le cadre du <a href="https://lacito.hypotheses.org/251" target="_blank">projet
+            </a><span style="font-style: italic;"><a href="https://lacito.hypotheses.org/251" target="_blank">DO-RE-MI-FA</a>
+(Sept. 2014-Feb. 2016) </span>financé
+par la&nbsp;<a href="http://www.bibliothequescientifiquenumerique.fr/" target="_blank">Bibliothèque Scientifique
+Numérique</a><span style="font-style: italic;">
+(BSN), un programme du Ministère de l'Enseignement
+Supérieur et de la Recherche (MESR, France)</span><i>.</i><i></i><span style="font-style: italic;"></span><span style="font-style: italic;"></span><span style="font-style: italic;"></span></p>
+            <a href="http://www.bibliothequescientifiquenumerique.fr/" target="_blank"> <br>
+            </a>
+            <table style="width: 800px; height: 90px; text-align: left; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="2">
+              <tbody>
+                <tr>
+                  <td style="text-align: center; height: 35%; width: 35%;"><a href="http://www.bibliothequescientifiquenumerique.fr/"><span style="text-decoration: underline;"><img style="border: 0px solid ; width: 241px; height: 64px;" alt="BSN" src="http://bbf.enssib.fr/consulter/bbf-2013-1-p61.jpg"></span></a></td>
+                  <td style="text-align: center;"><a href="http://www.mica.edu.vn/index.php"><img style="border: 0px solid ; width: 80px; height: 74px;" alt="MICA" src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Logo-mica-transp.gif"></a></td>
+                  <td style="text-align: center;">
+                  <p><a href="http://www.mica.edu.vn/research-actions/au-co-project/"><img style="border: 0px solid ; width: 109px; height: 50px;" alt="AuCo" src="https://upload.wikimedia.org/wikipedia/commons/f/ff/Logo_of_the_AuCo_collection%2C_a_project_of_the_MICA_International_Research_Institute_in_Hanoi%2C_Vietnam.jpg"></a></p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p style="font-style: italic;"><span style="font-style: italic;"></span></p>
+            <p>&nbsp;</p>
+            <p>Mise à
+jour : 2019.</p>
+            <p><a href="../../membres/michaud.htm">Contact: Alexis
+Michaud</a></p>
+</div>
+</body></html>
---
+++ Pangloss_Website/languages/Phong_en.php
@@ -0,0 +1,127 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head><meta charset="utf-8"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><div>&nbsp;</div>
+
+
+            <div class="page-header"><h1>Hung corpus (dialects: Toum, Ly Hà, Tày Poọng, and Phong) &nbsp;</h1></div>
+             <div class="text-left"> 
+            <p class="intertitre"> <a href="#Vietnamese"><span style="font-style: italic;">Giới
+thiệu bằng tiếng Việt</span></a><br>
+            </p>
+            
+<h4 style="font-style: italic;" align="left">This data set became public in 2019, under a Creative Commons license.</h4>
+<br>
+<span style="font-weight: normal;">About
+the Hung subgroup of Vietic, see pp. 15-16 of:
+Ferlus, Michel. 1996. </span><a style="font-weight: normal;" href="http://sealang.net/sala/archives/pdf8/ferlus1996langues.pdf" target="_blank">Langues et peuples viet-muong</a><span style="font-weight: normal;">. </span><span style="font-style: italic; font-weight: normal;">Mon-Khmer
+Studies</span> 26. 7–2 
+            <p class="intertitre"><strong>The
+archived documents: vocabulary</strong></p>
+            <p>The recordings made
+available here were conducted by Michel Ferlus, in collaboration with
+Pr. Trần Trí Dõi (Vietnam National University).
+They&nbsp;contain lexical data. As Ferlus's field notes have not
+been
+verified item by item to finalize the word list, it was decided to put
+the data online without a transcription, rather than run the risk of
+publishing data that contain mistaken
+transcriptions. Michel Ferlus rated the technical quality of the
+Aheu recordings as "very poor"; this is
+due to the use of low-grade tapes (counterfeit tapes sold on the local
+market).</p>
+            <br>
+            <p style="font-weight: bold;">References
+and links to
+related resources:&nbsp;</p>
+Ferlus, Michel. 1996. <a href="http://sealang.net/sala/archives/pdf8/ferlus1996langues.pdf" target="_blank">Langues et peuples viet-muong</a>. <span style="font-style: italic;">Mon-Khmer
+Studies</span> 26, pp.
+7–28.<br>
+            <span style="font-style: italic;"></span>
+            <p>The
+EFEO-CNRS-SOAS
+Word List for Linguistic Fieldwork in
+Southeast Asia, used to elicit the vocabulary, is available <a href="https://halshs.archives-ouvertes.fr/halshs-01068533/" target="_blank">here</a>.</p>
+            <p></p>
+            <p class="intertitre"><span style="font-style: italic;">Classification
+of the
+Vietic languages (a.k.a. Viet-Muong, Việt-Chứt)
+after Ferlus 1996, with links to the corresponding pages in the
+Pangloss Collection:</span>&nbsp;</p>
+            <table style="text-align: left;" ;="" border="1" cellpadding="2" cellspacing="2" width="80%">
+              <tbody>
+                <tr>
+                  <td align="undefined" valign="undefined">1. <a href="Maleng_en.htm" target="_blank">Maleng</a></td>
+                  <td align="undefined" valign="undefined">2. <a href="Arem_en.htm" target="_blank">Arem</a></td>
+                  <td align="undefined" valign="undefined">3. <a href="Chut_en.htm" target="_blank">Chứt</a></td>
+                  <td align="undefined" valign="undefined">4. <a href="Aheu_en.htm" target="_blank">Aheu</a></td>
+                  <td align="undefined" valign="undefined">5. Hung
+(this page)</td>
+                  <td align="undefined" valign="undefined">6. <a href="Tho_en.htm" target="_blank">Thổ</a></td>
+                  <td align="undefined" valign="undefined">7. <a href="Muong_en.htm" target="_blank">Mường</a></td>
+                  <td align="undefined" valign="undefined">8. <a href="Vietnamese_en.htm" target="_blank">Vietnamese</a></td>
+                </tr>
+              </tbody>
+            </table>
+            <p class="intertitre"></p>
+            <p></p>
+             <p style="font-style: italic; text-align: justify;">These
+materials&nbsp;were digitized by <span style="font-style: italic;">the <a href="http://www.mica.edu.vn/index.php" target="_blank">International
+Research Institute MICA</a> (</span><i>HUST
+– CNRS/UMI-2954 – Grenoble INP</i><span style="font-style: italic;">) in&nbsp;</span><span style="font-style: italic;">partnership</span><span style="font-style: italic;"> with <a href="http://lacito.vjf.cnrs.fr/" target="_blank">CNRS-LACITO</a>,
+            </span>as part of the
+DO-RE-MI-FA
+project <span style="font-style: italic;">(Sept.
+2014-Feb. 2016), </span>funded
+by the <a href="http://www.bibliothequescientifiquenumerique.fr/" target="_blank">Digital Scientific Library</a><span style="font-style: italic;">, a programme of the
+French Ministry for Higher
+Education and Research</span><span style="font-style: italic;">.</span><span style="font-style: italic;"></span><span style="font-style: italic;">&nbsp;</span></p>
+            <a href="http://www.bibliothequescientifiquenumerique.fr/" target="_blank"> <br>
+            </a>
+            <table style="width: 800px; height: 90px; text-align: left; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="2">
+              <tbody>
+                <tr>
+                  <td style="text-align: center; height: 35%; width: 35%;"><a href="http://www.bibliothequescientifiquenumerique.fr/"><span style="text-decoration: underline;"><img style="border: 0px solid ; width: 241px; height: 64px;" alt="BSN" src="http://bbf.enssib.fr/consulter/bbf-2013-1-p61.jpg"></span></a></td>
+                  <td style="text-align: center;"><a href="http://www.mica.edu.vn/index.php"><img style="border: 0px solid ; width: 80px; height: 74px;" alt="MICA" src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Logo-mica-transp.gif"></a></td>
+                  <td style="text-align: center;">
+                  <p><a href="http://www.mica.edu.vn/research-actions/au-co-project/"><img style="border: 0px solid ; width: 109px; height: 50px;" alt="AuCo" src="https://upload.wikimedia.org/wikipedia/commons/f/ff/Logo_of_the_AuCo_collection%2C_a_project_of_the_MICA_International_Research_Institute_in_Hanoi%2C_Vietnam.jpg"></a></p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p>Last updated 2019.</p>
+            <p><a href="../../membres/michaud.htm">Contact: Alexis
+Michaud</a></p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <h1 align="center"><a name="Vietnamese"></a>Ngữ
+liệu phương ngữ Hung</h1>
+            <strong>Các
+tài liệu được lưu trữ: ngữ liệu về từ
+vựng</strong>
+            <p>Phần ghi
+âm ở đây bao gồm các dữ liệu về từ vựng.
+Tài liệu phương ngữ Tày Poọng được ghi âm trong sự
+hợp tác với
+GS. TS. Trần
+Trí Dõi (VNU).</p>
+            <br>
+            <p style="font-weight: bold;">Tài
+liệu tham khảo:&nbsp;</p>
+Bảng từ
+EFEO-CNRS-SOAS dùng trong công tác điền
+dã ngôn ngữ học ở Đông Nam Á
+(tải bảng từ <a href="https://halshs.archives-ouvertes.fr/halshs-01068533/" target="_blank">ở đây</a>). <br>
+            <br>
+Các
+tư liệu về tiếng Hung đã được số hóa
+và biên tập lại. Đây là một
+phần của dự án kéo dài từ
+tháng 9/2014 đến tháng 2/2016, với ngân
+sách từ chương trình Thư viện Khoa học Kỹ thuật
+số <a href="http://www.bibliothequescientifiquenumerique.fr/" target="_blank">(Digital Scientific Library)</a><span style="font-style: italic;">, một chương
+trình của Bộ Giáo dục đại học và
+Nghiên cứu Pháp.&nbsp;</span> <a href="http://www.bibliothequescientifiquenumerique.fr/" target="_blank">
+            <img src="../../images/logos/bsn.png" alt="Digital Scientific Library" border="0" height="75"></a>
+            <p style="font-style: italic;"><span style="font-style: italic;"></span></p>
+			</div>
+</body></html>
